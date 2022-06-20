a3bf4b672415a16108f44cc464e7b6012f87f4ae
Mouhcine Karrit <mouhcine.karrit@cdbdx.biz>
2022-06-20 11:29:23 +0200

    Add portfolio And TESTs

---
+++ XtremTDDMoneyKata/money-problem.Domain/Portfolio.cs
@@ -0,0 +1,20 @@
+﻿using money_problem.Domain;
+
+namespace money_problem.Tests
+{
+    public class Portfolio
+    {
+        public Portfolio()
+        {
+        }
+
+        public void Add(Currency currency, double amount)
+        {
+        }
+
+        public double Evaluate(Currency currency)
+        {
+            return 17;
+        }
+    }
+}
---
+++ XtremTDDMoneyKata/money-problem.Tests/PortefolioTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using static money_problem.Domain.Currency;
+
+namespace money_problem.Tests
+{
+    public class PortefolioTests
+    {
+        [Fact(DisplayName = "5 USD + 10 EUR = 17 USD")]
+        public void AddEurosToUSD()
+        {
+            Portfolio portfolio = new();
+            portfolio.Add(USD, 5);
+            portfolio.Add(EUR, 10);
+            double expectedAmount = portfolio.Evaluate(USD);
+            Assert.Equal(17, expectedAmount);
+        }
+
+        [Fact(DisplayName = "1 USD + 1100 KRW = 2200 KRW")]
+        public void AddUSDToKRW()
+        {
+            Portfolio portfolio = new();
+            portfolio.Add(USD, 1);
+            portfolio.Add(KRW, 1100);
+            double expectedAmount = portfolio.Evaluate(KRW);
+            Assert.Equal(2200, expectedAmount);
+        }
+    }
+}
