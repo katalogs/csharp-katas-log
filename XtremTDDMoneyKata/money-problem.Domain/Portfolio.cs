b9f99fc1fb1c6fd7f8e862dd79fa338540beacf4
Julien Lafargue <julien.lafargue@cdbdx.biz>
2022-06-20 11:44:24 +0200

    Add Bank to portfolio constructor Add tests

--- XtremTDDMoneyKata/money-problem.Domain/Portfolio.cs
+++ XtremTDDMoneyKata/money-problem.Domain/Portfolio.cs
@@ -4,17 +4,29 @@ namespace money_problem.Tests
 {
     public class Portfolio
     {
-        public Portfolio()
+        private IDictionary<Currency, double> _moneys = new Dictionary<Currency, double>();
+
+        private readonly Bank _bank;
+
+        public Portfolio(Bank bank)
         {
+            _bank = bank;
         }
 
         public void Add(Currency currency, double amount)
         {
+            _moneys.Add(currency, amount);
         }
 
         public double Evaluate(Currency currency)
         {
-            return 17;
+            double totalAmount = 0;
+
+            foreach (var money in _moneys )
+            {
+                totalAmount += _bank.Convert(money.Value, money.Key, currency);
+            }
+            return totalAmount;
         }
     }
 }
--- XtremTDDMoneyKata/money-problem.Tests/PortefolioTests.cs
+++ XtremTDDMoneyKata/money-problem.Tests/PortefolioTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using money_problem.Domain;
+using Xunit;
 using static money_problem.Domain.Currency;
 
 namespace money_problem.Tests
@@ -8,7 +9,7 @@ namespace money_problem.Tests
         [Fact(DisplayName = "5 USD + 10 EUR = 17 USD")]
         public void AddEurosToUSD()
         {
-            Portfolio portfolio = new();
+            Portfolio portfolio = new(Bank.WithExchangeRate(EUR, USD, 1.2));
             portfolio.Add(USD, 5);
             portfolio.Add(EUR, 10);
             double expectedAmount = portfolio.Evaluate(USD);
@@ -18,7 +19,7 @@ namespace money_problem.Tests
         [Fact(DisplayName = "1 USD + 1100 KRW = 2200 KRW")]
         public void AddUSDToKRW()
         {
-            Portfolio portfolio = new();
+            Portfolio portfolio = new(Bank.WithExchangeRate(USD, KRW, 1100));
             portfolio.Add(USD, 1);
             portfolio.Add(KRW, 1100);
             double expectedAmount = portfolio.Evaluate(KRW);
