328a23438ad48ea788353689b5db43c876984a0f
Alexandre Plaitant <alexandre.plaitant@cdiscount.com>
2022-07-04 11:39:32 +0200

    WIP: Refacto TU

--- XtremTDDMoneyKata/money-problem.Tests/BankShould.cs
+++ XtremTDDMoneyKata/money-problem.Tests/BankShould.cs
@@ -20,15 +20,15 @@ namespace money_problem.Tests
         [Fact(DisplayName = "10 EUR -> EUR = 10 EUR")]
         public void ConvertMoneyInSameCurrency()
         {
-            _bank.Convert(10, EUR, EUR)
+            _bank.Convert(new Money(10, EUR), EUR)
                 .Should()
-                .Be(10);
+                .BeEquivalentTo(new Money(10, EUR));
         }
 
         [Fact(DisplayName = "Throws a MissingExchangeRateException in case of missing exchange rates")]
         public void ConvertWithMissingExchangeRateShouldThrowException()
         {
-            _bank.Invoking(_ => _.Convert(10, EUR, KRW))
+            _bank.Invoking(_ => _.Convert(new Money(10, EUR), KRW))
                 .Should()
                 .ThrowExactly<MissingExchangeRateException>()
                 .WithMessage("EUR->KRW");
@@ -37,15 +37,15 @@ namespace money_problem.Tests
         [Fact(DisplayName = "Conversion with different exchange rates EUR -> USD")]
         public void ConvertWithDifferentExchangeRates()
         {
-            _bank.Convert(10, EUR, USD)
+            _bank.Convert(new Money(10, EUR), USD)
                 .Should()
-                .Be(12);
+                .BeEquivalentTo(new Money(12, USD));
 
             _bank.AddExchangeRate(EUR, USD, 1.3);
             
-            _bank.Convert(10, EUR, USD)
+            _bank.Convert(new Money(10, EUR), USD)
                 .Should()
-                .Be(13);
+                .BeEquivalentTo(new Money(13, USD));
         }
     }
 }
