c72288c654605ba205cb0d71d364c67beacfad0a
Alexandre Plaitant <alexandre.plaitant@cdiscount.com>
2022-10-03 12:00:11 +0200

    Override method to bypass static method

--- TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
+++ TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using TripServiceKata.Exception;
 using TripServiceKata.Trip;
 
 namespace TripServiceKata.Tests
@@ -8,9 +10,77 @@ namespace TripServiceKata.Tests
     public class TripServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void UserNotLoggedInThrowAnException()
         {
-            TripService tripService = new TripService();
+            var user = new User.User();
+            TripService tripServiceWithoutLoggedInUser = new TestableTripService(default);
+
+            Assert.ThrowsException<UserNotLoggedInException>(() =>
+                tripServiceWithoutLoggedInUser.GetTripsByUser(user));
+        }
+        
+        [TestMethod]
+        public void UserWithoutFriends()
+        {
+            var userWithoutFriends = new User.User();
+            TripService tripServiceLoggedInUser = new TestableTripService(new User.User());
+
+            var result =
+                tripServiceLoggedInUser.GetTripsByUser(userWithoutFriends);
+
+            result.Should().BeEmpty();
+        }
+        
+        [TestMethod]
+        public void UserWithFriendsButWihtNotLoggedInUser()
+        {
+            var userWithFriends = new User.User();
+            User.User aFriend = new User.User();
+            userWithFriends.AddFriend(aFriend);
+            User.User loggedInUser = new User.User();
+            TripService tripServiceLoggedInUser = new TestableTripService(loggedInUser);
+
+            var result =
+                tripServiceLoggedInUser.GetTripsByUser(userWithFriends);
+
+            result.Should().BeEmpty();
+        }
+        
+        [TestMethod]
+        public void UserWithFriendsWihtLoggedInUser()
+        {
+            var userWithFriends = new User.User();
+            User.User loggedInUser = new User.User();
+            userWithFriends.AddFriend(loggedInUser);
+            Trip.Trip trip = new Trip.Trip();
+            userWithFriends.AddTrip(trip);
+            TripService tripServiceLoggedInUser = new TestableTripService(loggedInUser);
+
+            var result =
+                tripServiceLoggedInUser.GetTripsByUser(userWithFriends);
+
+            result.Should().NotBeEmpty();
+            result.Should().Contain(trip);
+        }
+
+        public class TestableTripService : TripService
+        {
+            private User.User loggedInUser;
+
+            public TestableTripService(User.User inUser)
+            {
+                loggedInUser = inUser;
+            }
+
+            protected override User.User GetLoggedUser()
+            {
+                return loggedInUser;
+            }
+
+            protected override List<Trip.Trip> FindTripsByUser(User.User user)
+            {
+                return user.Trips();
+            }
         }
     }
 }
--- TripServiceKata/TripServiceKata/Trip/TripService.cs
+++ TripServiceKata/TripServiceKata/Trip/TripService.cs
@@ -9,7 +9,7 @@ namespace TripServiceKata.Trip
         public List<Trip> GetTripsByUser(User.User user)
         {
             List<Trip> tripList = new List<Trip>();
-            User.User loggedUser = UserSession.GetInstance().GetLoggedUser();
+            User.User loggedUser = GetLoggedUser();
             bool isFriend = false;
             if (loggedUser != null)
             {
@@ -23,7 +23,7 @@ namespace TripServiceKata.Trip
                 }
                 if (isFriend)
                 {
-                    tripList = TripDAO.FindTripsByUser(user);
+                    tripList = FindTripsByUser(user);
                 }
                 return tripList;
             }
@@ -31,6 +31,16 @@ namespace TripServiceKata.Trip
             {
                 throw new UserNotLoggedInException();
             }
+        }
+
+        protected virtual List<Trip> FindTripsByUser(User.User user)
+        {
+            return TripDAO.FindTripsByUser(user);
+        }
+
+        protected virtual User.User GetLoggedUser()
+        {
+            return UserSession.GetInstance().GetLoggedUser();
         }
     }
 }
