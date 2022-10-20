0f6bf5544a93f308d52b08f0a5882d9766e38aeb
gaspard.pointeau <gaspard.pointeau@gmail.com>
2022-10-20 14:46:02 +0200

    add tests

---
+++ TripServiceKata/TripServiceKata.Tests/TripServiceTest.TestableTripService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using TripServiceKata.Trip;
+
+namespace TripServiceKata.Tests
+{
+
+    public partial class TripServiceTest
+    {
+        public class TestableTripService : TripService
+        {
+            private readonly User.User _loggedInUser;
+
+            public TestableTripService(User.User loggedInUser)
+                => _loggedInUser = loggedInUser;
+
+            protected override User.User GetLoggedUser()
+                => _loggedInUser;
+
+            protected override List<Trip.Trip> FindTripsByUser(User.User user)
+                => user.Trips();
+        }
+    }
+
+}
--- TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
+++ TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
@@ -1,16 +1,73 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using TripServiceKata.Exception;
 using TripServiceKata.Trip;
+using TripServiceKata.User;
 
 namespace TripServiceKata.Tests
 {
+
     [TestClass]
-    public class TripServiceTest
+    public partial class TripServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Should_Throw_an_Exception_when_noOne_is_logged_in()
         {
-            TripService tripService = new TripService();
+            User.User loggedInUser = null;
+            TripService service = new TestableTripService(loggedInUser);
+
+            User.User aUser = new User.User();
+
+            Assert.ThrowsException<UserNotLoggedInException>(() => service.GetTripsByUser(aUser));
         }
+
+        [TestMethod]
+        public void Should_return_empty_list_when_user_has_no_friends()
+        {
+            User.User loggedInUser = new User.User();
+            TripService service = new TestableTripService(loggedInUser);
+
+            User.User aUserWithoutFriends = new User.User();
+
+            List<Trip.Trip> trips = service.GetTripsByUser(aUserWithoutFriends);
+
+            Assert.IsTrue(!trips.Any());
+        }
+
+        [TestMethod]
+        public void Should_return_empty_list_when_user_and_loggedIn_are_not_friends()
+        {
+            User.User loggedInUser = new User.User();
+            TripService service = new TestableTripService(loggedInUser);
+
+            User.User aUser = new User.User();
+            User.User aFriend = new User.User();
+            aUser.AddFriend(aFriend);
+            Trip.Trip tripsA = new Trip.Trip();
+            aUser.AddTrip(tripsA);
+
+            List<Trip.Trip> trips = service.GetTripsByUser(aUser);
+
+            Assert.IsTrue(!trips.Any());
+        }
+
+        [TestMethod]
+        public void Should_return_specific_trips_when_loggedIn_and_User_are_friends()
+        {
+            User.User loggedInUser = new User.User();
+            TripService service = new TestableTripService(loggedInUser);
+
+            User.User aUser = new User.User();
+            aUser.AddFriend(loggedInUser);
+            Trip.Trip tripsA = new Trip.Trip();
+            aUser.AddTrip(tripsA);
+
+            List<Trip.Trip> trips = service.GetTripsByUser(aUser);
+
+            Assert.IsTrue(trips.Contains(tripsA));
+        }
+
     }
+
 }
