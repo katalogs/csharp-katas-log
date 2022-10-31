b539df592952bdbbff35b3fa75c36909bbea89c1
Taha Amine <taha.amine@cdbdx.biz>
2022-10-31 11:45:19 +0100

    adding approval testing

--- GildedRose/GildedRoseTests/GildedRoseTest.cs
+++ GildedRose/GildedRoseTests/GildedRoseTest.cs
@@ -1,19 +1,27 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
+
+using Argon;
+
 using GildedRoseKata;
-using Xunit;
 
 namespace GildedRoseTests
 {
+    [UsesVerify]
     public class GildedRoseTest
     {
+        public GildedRoseTest()
+        {
+            VerifierSettings.AddExtraSettings(serializerSettings =>
+                serializerSettings.DefaultValueHandling = DefaultValueHandling.Include);
+        }
+
         [Fact]
-        public void foo()
+        public Task UpdateQuality_WithNominalItem_ShouldHaveCorrectValue()
         {
             IList<Item> Items = new List<Item> {new() {Name = "foo", SellIn = 0, Quality = 0}};
             var gildedRose = new GildedRose(Items);
             gildedRose.UpdateQuality();
-            Assert.Equal("foo", Items[0].Name);
+            return Verify(Items);
+            /*Assert.Equal("foo", Items[0].Name);
             Assert.Equal(-1, Items[0].SellIn);
             Assert.Equal(0, Items[0].Quality);*/
         }
