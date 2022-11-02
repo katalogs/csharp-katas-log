40b3f7c0f0efe3ea9a6a6bde9a8b7716eb05ba27
Alexis Vuk <alexis.vuk@cdbdx.biz>
2022-11-02 14:23:02 +0100

    add deposite test with 10

--- BankingKata/Banking.Tests.Unit/BankingTest.cs
+++ BankingKata/Banking.Tests.Unit/BankingTest.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 
 namespace Banking.Tests.Unit
@@ -7,9 +6,19 @@ namespace Banking.Tests.Unit
     public class BankingTest
     {
         [Fact]
-        public void Test1()
-        {
+        public void Deposite_10_Euro_Should_Add_10_To_Balance(){
+            
+            //Arrange
+            const int money = 10;
+            const int balance = 0;
+            const string accountToSend = "accountToDepose";
+            Account account = new Account(balance);
+            
+            //Act
+            var result = account.SendMoney(money, accountToSend);
+            
+            //Assert
+            Assert.Equal(result, money);
         }
     }
-
 }
--- BankingKata/Banking/Account.cs
+++ BankingKata/Banking/Account.cs
@@ -5,6 +5,15 @@ namespace Banking
 
     public class Account
     {
+        
+        public Account(int balance)
+        {
+            
+        }
+        public int SendMoney(int money, string account)
+        {
+            return 0;
+        }
     }
 
 }
