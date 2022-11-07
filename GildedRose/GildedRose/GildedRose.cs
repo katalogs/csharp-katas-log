c384eb3327400b304783a529bfe3fabaaaa30797
Taha Amine <taha.amine@cdbdx.biz>
2022-11-07 12:23:28 +0100

    Backstage passes to a TAFKAL80ETC concert  refacto

--- GildedRose/GildedRose/GildedRose.cs
+++ GildedRose/GildedRose/GildedRose.cs
@@ -35,7 +35,37 @@ namespace GildedRoseKata
                 }
                 else
                 {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (item.Quality < 50)
+                        {
+                            item.Quality++;
+
+                            if (item.SellIn < 11)
+                            {
+                                if (item.Quality < 50)
+                                {
+                                    item.Quality++;
+                                }
+                            }
+
+                            if (item.SellIn < 6)
+                            {
+                                if (item.Quality < 50)
+                                {
+                                    item.Quality++;
+                                }
+                            }
+                        }
+
+                        item.SellIn--;
+
+                        if (item.SellIn < 0)
+                        {
+                            item.Quality = 0;
+                        }
+                    }
+                    else
                     {
                         if (item.Quality > 0)
                         {
@@ -44,42 +74,13 @@ namespace GildedRoseKata
                                 item.Quality--;
                             }
                         }
-                    }
-                    else
-                    {
-                        if (item.Quality < 50)
+
+                        if (item.Name != "Sulfuras, Hand of Ragnaros")
                         {
-                            item.Quality++;
-
-                            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                            {
-                                if (item.SellIn < 11)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality++;
-                                    }
-                                }
-
-                                if (item.SellIn < 6)
-                                {
-                                    if (item.Quality < 50)
-                                    {
-                                        item.Quality++;
-                                    }
-                                }
-                            }
+                            item.SellIn--;
                         }
-                    }
 
-                    if (item.Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        item.SellIn--;
-                    }
-
-                    if (item.SellIn < 0)
-                    {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.SellIn < 0)
                         {
                             if (item.Quality > 0)
                             {
@@ -89,14 +90,9 @@ namespace GildedRoseKata
                                 }
                             }
                         }
-                        else
-                        {
-                            item.Quality = 0;
-                        }
                     }
                 }
             }
         }
     }
-
 }
