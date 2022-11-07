0516057357d18dead8293bbf610b0cbcb7b9f070
Taha Amine <taha.amine@cdbdx.biz>
2022-11-07 11:22:04 +0100

    clean test code

--- GildedRose/GildedRoseTests/GildedRoseTest.cs
+++ GildedRose/GildedRoseTests/GildedRoseTest.cs
@@ -21,10 +21,7 @@ namespace GildedRoseTests
             IList<Item> Items = new List<Item> {new() {Name = "foo", SellIn = 0, Quality = 0}};
             var gildedRose = new GildedRose(Items);
             gildedRose.UpdateQuality();
-            return Verify(Items);
-            /*Assert.Equal("foo", Items[0].Name);
-            Assert.Equal(-1, Items[0].SellIn);
-            Assert.Equal(0, Items[0].Quality);*/
+            return Verify(Items);           
         }
 
         [Fact]
