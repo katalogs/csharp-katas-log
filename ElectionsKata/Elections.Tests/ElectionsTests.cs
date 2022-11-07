781b6af41e47793015d87a7b870c9834c21c0afc
Julien Lafargue <julien.lafargue@cdbdx.biz>
2022-11-07 11:28:31 +0100

    Add approval tests

--- ElectionsKata/Elections.Tests/ElectionsTests.cs
+++ ElectionsKata/Elections.Tests/ElectionsTests.cs
@@ -1,43 +1,46 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using VerifyXunit;
 using Xunit;
+using static VerifyXunit.Verifier;
+
 
 namespace Elections.Tests;
 
+[UsesVerify]
 public class ElectionsTests
 {
     [Fact]
-    public void Should_run_without_districts()
+    public Task Should_run_without_districts()
+    {
+        var list = new Dictionary<string, List<string>>
         {
-            var list = new Dictionary<string, List<string>>
-            {
-                ["District 1"] = new List<string> {"Bob", "Anna", "Jess", "July"},
-                ["District 2"] = new List<string> {"Jerry", "Simon"},
-                ["District 3"] = new List<string> {"Johnny", "Matt", "Carole"}
-            };
+            ["District 1"] = new List<string> {"Bob", "Anna", "Jess", "July"},
+            ["District 2"] = new List<string> {"Jerry", "Simon"},
+            ["District 3"] = new List<string> {"Johnny", "Matt", "Carole"}
+        };
 
-            var elections = new Elections(list, false);
-            elections.AddCandidate("Michel");
-            elections.AddCandidate("Jerry");
-            elections.AddCandidate("Johnny");
+        var elections = new Elections(list, false);
+        elections.AddCandidate("Michel");
+        elections.AddCandidate("Jerry");
+        elections.AddCandidate("Johnny");
 
-            elections.VoteFor("Bob", "Jerry", "District 1");
-            elections.VoteFor("Jerry", "Jerry", "District 2");
-            elections.VoteFor("Anna", "Johnny", "District 1");
-            elections.VoteFor("Johnny", "Johnny", "District 3");
-            elections.VoteFor("Matt", "Donald", "District 3");
-            elections.VoteFor("Jess", "Joe", "District 1");
-            elections.VoteFor("Simon", "", "District 2");
-            elections.VoteFor("Carole", "", "District 3");
+        elections.VoteFor("Bob", "Jerry", "District 1");
+        elections.VoteFor("Jerry", "Jerry", "District 2");
+        elections.VoteFor("Anna", "Johnny", "District 1");
+        elections.VoteFor("Johnny", "Johnny", "District 3");
+        elections.VoteFor("Matt", "Donald", "District 3");
+        elections.VoteFor("Jess", "Jerry", "District 1");
+        elections.VoteFor("Simon", "", "District 2");
+        elections.VoteFor("Carole", "", "District 3");
 
-            var results = elections.Results();
+        var results = elections.Results();
 
-
-            // Add approval tests here
-
-        }
+        return Verify(results);
+    }
 
         [Fact]
-        public void Should_run_with_districts()
+        public Task Should_run_with_districts()
         {
             var list = new Dictionary<string, List<string>>
             {
@@ -54,14 +57,14 @@ public class ElectionsTests
             elections.VoteFor("Jerry", "Jerry", "District 2");
             elections.VoteFor("Anna", "Johnny", "District 1");
             elections.VoteFor("Johnny", "Johnny", "District 3");
-            elections.VoteFor("Matt", "Donald", "District 3");
-            elections.VoteFor("Jess", "Joe", "District 1");
+            elections.VoteFor("Matt", "NULL", "District 3");
+            elections.VoteFor("Jess", "NULL2", "District 1");
             elections.VoteFor("July", "Jerry", "District 1");
             elections.VoteFor("Simon", "", "District 2");
-            elections.VoteFor("Carole", "", "District 3");
+            elections.VoteFor("Carole", "NULL3", "District 3");
 
             var results = elections.Results();
 
-            // Add approval tests here
+            return Verify(results);
         }
 }
