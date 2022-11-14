241bbfa94016a3cc6dad48bd56e1cfb22f2d21ee
Lucas Consejo <lucas.consejo@cdbdx.biz>
2022-11-14 16:26:40 +0100

    Revert "Refacto order class"
    This reverts commit b852d41d6d975da55b6954e742b7535f56a25695.

--- OrderShippingKata/Order Shipping/Domain/Order.cs
+++ OrderShippingKata/Order Shipping/Domain/Order.cs
@@ -1,22 +1,20 @@
-﻿using Ardalis.GuardClauses;
-
-namespace OrderShipping.Domain
+﻿namespace OrderShipping.Domain
 {
     public class Order
     {
 
         public decimal Total { get; private set; }
         public string Currency { get; set; }
-        public IEnumerable<OrderItem> Items { get; set; }
+        public IList<OrderItem> Items { get; set; }
         public decimal Tax { get; private set; }
         public OrderStatus Status { get; set; }
         public int Id { get; set; }
 
-        public Order(List<OrderItem> items)
+        public Order()
         {
             Status = OrderStatus.Created;
+            Items = new List<OrderItem>();
             Currency = "EUR";
-            Items = Guard.Against.NullOrEmpty(items);
         }
 
         public void AddOrderItem(OrderItem orderItem)
