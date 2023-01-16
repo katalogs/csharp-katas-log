7490faf8b725f045ce5d7a79d41ac3b93cf0156e
Kévin Giraud <kevin.giraud@cdbdx.biz>
2023-01-16 17:33:44 +0100

    Number 0 to 9

--- RomanNumeralKata/RomanNumeral/ToFrenchConverter.cs
+++ RomanNumeralKata/RomanNumeral/ToFrenchConverter.cs
@@ -1,14 +1,31 @@
-﻿namespace RomanNumerals
+﻿using System.Collections.Generic;
+
+namespace RomanNumerals
 {
     public class ToFrenchConverter
     {
+        private Dictionary<int, string> dictionaryUnit =
+            new Dictionary<int, string>
+            {
+                {0, "Zéro"},
+                {1, "Un"},
+                {2, "Deux"},
+                {3, "Trois"},
+                {4, "Quatre"},
+                {5, "Cinq"},
+                {6, "Six"},
+                {7, "Sept"},
+                {8, "Huit"},
+                {9, "Neuf"},
+            };
+
         public ToFrenchConverter()
         {
         }
 
         public string Convert(int number)
         {
-            return "Zéro";
+            return dictionaryUnit[number];
         }
     }
 }
--- RomanNumeralKata/RomanNumerals.Test/ToFrenchConverterTest.cs
+++ RomanNumeralKata/RomanNumerals.Test/ToFrenchConverterTest.cs
@@ -13,5 +13,33 @@ namespace RomanNumerals.Test
 
             Assert.Equal("Zéro", res);
         }
+
+        [Fact]
+        public void When_1_ShouldReturns_Un()
+        {
+            var converter = new ToFrenchConverter();
+
+            string res = converter.Convert(1);
+
+            Assert.Equal("Un", res);
+        }
+
+        [Theory]
+        [InlineData(2, "Deux")]
+        [InlineData(3, "Trois")]
+        [InlineData(4, "Quatre")]
+        [InlineData(5, "Cinq")]
+        [InlineData(6, "Six")]
+        [InlineData(7, "Sept")]
+        [InlineData(8, "Huit")]
+        [InlineData(9, "Neuf")]
+        public void ShouldReturnsExpectedUnit(int unit, string expected)
+        {
+            var converter = new ToFrenchConverter();
+
+            string res = converter.Convert(unit);
+
+            Assert.Equal(expected, res);
+        }
     }
 }
