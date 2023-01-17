b4e250affff85d3bcbb24ef5612dac154b0229c6
Ahmed Amine Mahamani <ahmedamine.mahamani@cdbdx.biz>
2023-01-17 11:36:52 +0100

    test unitaire cheese expired

--- GildedRose/GildedRoseTests/CheeseTests.cs
+++ GildedRose/GildedRoseTests/CheeseTests.cs
@@ -7,13 +7,31 @@ namespace GildedRoseTests
         [Fact]
         public void Update_IncreaseQuality_By_One()
         {
-            var cheese = new Cheese("name");
-            cheese.SellIn = 5;
-            cheese.Quality = 2;
+            var cheese = new Cheese("name")
+            {
+                SellIn = 5,
+                Quality = 2
+            };
 
             cheese.Update();
 
             Assert.Equal(3, cheese.Quality);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public void Update_IncreaseQuality_WhenExpired_Twice(int sellIn)
+        {
+            var cheese = new Cheese("name")
+            {
+                SellIn = sellIn,
+                Quality = 2
+            };
+
+            cheese.Update();
+
+            Assert.Equal(4, cheese.Quality);
         }
     }
 }
