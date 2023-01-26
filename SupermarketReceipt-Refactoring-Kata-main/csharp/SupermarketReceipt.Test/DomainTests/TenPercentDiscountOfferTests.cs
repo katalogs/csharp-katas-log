1129ef3110b2142b88c2b703438fd53b7934e10a
SylvainJ66 <sylvain.jouglard.66@gmail.com>
2023-01-26 15:31:09 +0100

    [Refacto] - Add some tests on discount

--- SupermarketReceipt-Refactoring-Kata-main/csharp/SupermarketReceipt.Test/DomainTests/TenPercentDiscountOfferTests.cs
+++ SupermarketReceipt-Refactoring-Kata-main/csharp/SupermarketReceipt.Test/DomainTests/TenPercentDiscountOfferTests.cs
@@ -9,10 +9,31 @@ namespace SupermarketReceipt.Test.DomainTests
         [Fact]
         public void Should_be_applicable_when_1_product()
         {
-            var product = new Product("product1", ProductUnit.Kilo);
-            var offer = new TenPercentDiscountOffer(product);
-
+            var offer = PrepareOffer();
             Assert.True(offer.IsApplicable(1));
         }
+
+        private static TenPercentDiscountOffer PrepareOffer()
+        {
+            var offer = PrepareOffer();
+            return offer;
+        }
+
+        [Fact]
+        public void Should_create_discount_10_percent()
+        {
+            var offer = PrepareOffer();
+            var actual = offer.CreateDiscount(1, 100);
+            Assert.Equal(-10, actual.DiscountAmount);
+            Assert.Equal("10% off", actual.Description);
+        }
+
+        [Fact]
+        public void Should_not_be_applicable_when_less_than_one_product()
+        {
+            var offer = PrepareOffer();
+            Assert.False(offer.IsApplicable(0.5));
+        }
+
     }
 }
