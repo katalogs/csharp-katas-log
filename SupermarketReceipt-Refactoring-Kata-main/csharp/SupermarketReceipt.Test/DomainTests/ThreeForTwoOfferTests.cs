115241ebc2bed5420c5e36a68f28e67b4c08c5b4
Mourad4021 <mouradbenhlayem@gmail.com>
2023-01-26 16:03:04 +0100

    [TEST] add tests to Three for two offer

--- SupermarketReceipt-Refactoring-Kata-main/csharp/SupermarketReceipt.Test/DomainTests/ThreeForTwoOfferTests.cs
+++ SupermarketReceipt-Refactoring-Kata-main/csharp/SupermarketReceipt.Test/DomainTests/ThreeForTwoOfferTests.cs
@@ -6,33 +6,42 @@ namespace SupermarketReceipt.Test.DomainTests
 {
     public class ThreeForTwoOfferTests
     {
-        [Fact]
-        public void Should_be_applicable_when_3_product()
+        private readonly ThreeForTwoOffer _offer;
+
+        public ThreeForTwoOfferTests()
         {
             var product = new Product("product1", ProductUnit.Kilo);
-            var offer = new ThreeForTwoOffer(product);
-
-            Assert.True(offer.IsApplicable(3));
+            _offer  = new ThreeForTwoOffer(product);
         }
+        [Fact]
+        public void Should_be_applicable_when_3_product()
+            => Assert.True(_offer.IsApplicable(3));
 
         [Fact]
         public void Should_be_not_applicable_when_1_product()
-        {
-            var product = new Product("product1", ProductUnit.Kilo);
-            var offer = new ThreeForTwoOffer(product);
-
-            Assert.False(offer.IsApplicable(1));
-        }
+            => Assert.False(_offer.IsApplicable(1));
 
         [Fact]
         public void Should_create_discount()
         {
-            var product = new Product("product1", ProductUnit.Kilo);
-            var offer = new ThreeForTwoOffer(product);
-
-            var discount = offer.CreateDiscount(3, 2);
-
+            var discount = _offer.CreateDiscount(3, 2);
             Assert.Equal(-2, discount.DiscountAmount);
+        }
+
+        [Fact]
+        public void Should_create_discount_twice()
+        {
+            var actual = _offer.CreateDiscount(6, 2);
+            Assert.Equal(-2 * 2, actual.DiscountAmount);
+            Assert.Equal("3 for 2", actual.Description);
+        }
+
+        [Fact]
+        public void Should_create_only_one_discount()
+        {
+            var actual = _offer.CreateDiscount(4, 2);
+            Assert.Equal(-2, actual.DiscountAmount);
+            Assert.Equal("3 for 2", actual.Description);
         }
     }
 }
