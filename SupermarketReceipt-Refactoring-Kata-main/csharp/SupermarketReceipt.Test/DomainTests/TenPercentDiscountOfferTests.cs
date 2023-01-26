5209daa4d42ecd272828af441a46c194f9f13b6a
Mourad4021 <mouradbenhlayem@gmail.com>
2023-01-26 15:37:44 +0100

    [REFACTO] TenPercentDiscountOfferTests

--- SupermarketReceipt-Refactoring-Kata-main/csharp/SupermarketReceipt.Test/DomainTests/TenPercentDiscountOfferTests.cs
+++ SupermarketReceipt-Refactoring-Kata-main/csharp/SupermarketReceipt.Test/DomainTests/TenPercentDiscountOfferTests.cs
@@ -6,34 +6,28 @@ namespace SupermarketReceipt.Test.DomainTests
 {
     public class TenPercentDiscountOfferTests
     {
-        [Fact]
-        public void Should_be_applicable_when_1_product()
+        private readonly TenPercentDiscountOffer _offer;
+
+        public TenPercentDiscountOfferTests()
         {
-            var offer = PrepareOffer();
-            Assert.True(offer.IsApplicable(1));
+            var product = new Product("product1", ProductUnit.Kilo);
+            _offer = new TenPercentDiscountOffer(product);
         }
 
-        private static TenPercentDiscountOffer PrepareOffer()
-        {
-            var offer = PrepareOffer();
-            return offer;
-        }
+        [Fact]
+        public void Should_be_applicable_when_1_product()
+            => Assert.True(_offer.IsApplicable(1));
+
+        [Fact]
+        public void Should_not_be_applicable_when_less_than_one_product()
+            => Assert.False(_offer.IsApplicable(0.5));
 
         [Fact]
         public void Should_create_discount_10_percent()
         {
-            var offer = PrepareOffer();
-            var actual = offer.CreateDiscount(1, 100);
+            var actual = _offer.CreateDiscount(1, 100);
             Assert.Equal(-10, actual.DiscountAmount);
             Assert.Equal("10% off", actual.Description);
         }
-
-        [Fact]
-        public void Should_not_be_applicable_when_less_than_one_product()
-        {
-            var offer = PrepareOffer();
-            Assert.False(offer.IsApplicable(0.5));
-        }
-
     }
 }
