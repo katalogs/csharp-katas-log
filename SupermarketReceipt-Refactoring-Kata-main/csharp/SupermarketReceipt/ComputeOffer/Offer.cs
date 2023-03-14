22f50a0b1a82329415976b122f43584fe13b2141
Michaël Naxara <michael.naxara@cdbdx.biz>
2023-03-14 11:33:54 +0100

    move createDiscount to Offer

--- SupermarketReceipt-Refactoring-Kata-main/csharp/SupermarketReceipt/ComputeOffer/Offer.cs
+++ SupermarketReceipt-Refactoring-Kata-main/csharp/SupermarketReceipt/ComputeOffer/Offer.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace SupermarketReceipt.ComputeOffer
 {
     public enum SpecialOfferType
@@ -10,6 +12,8 @@ namespace SupermarketReceipt.ComputeOffer
 
     public class Offer
     {
+        private static readonly CultureInfo Culture = CultureInfo.CreateSpecificCulture("en-GB");
+
         private Product _product;
 
         public Offer(SpecialOfferType offerType, Product product, double argument)
@@ -33,6 +37,61 @@ namespace SupermarketReceipt.ComputeOffer
             };
 
             return quantityAsInt >= nbOfProductNecessaryForOffer;
+        }
+
+        public Discount CreateDiscount(double quantity, double unitPrice)
+        {
+            Discount discount = null;
+            var quantityAsInt = (int)quantity;
+
+            var nbOfProductNecessaryForOffer = OfferType switch
+            {
+                SpecialOfferType.ThreeForTwo => 3,
+                SpecialOfferType.TwoForAmount => 2,
+                SpecialOfferType.FiveForAmount => 5,
+                _ => 1
+            };
+
+            if (IsApplicable(quantityAsInt))
+            {
+                int nbOfPacks = quantityAsInt / nbOfProductNecessaryForOffer;
+                switch (OfferType)
+                {
+                    case SpecialOfferType.ThreeForTwo:
+                        {
+                            var discountAmount = quantity * unitPrice - (nbOfPacks * 2 * unitPrice + quantityAsInt % 3 * unitPrice);
+                            discount = new Discount(_product, "3 for 2", -discountAmount);
+
+                            break;
+                        }
+                    case SpecialOfferType.TenPercentDiscount:
+                        discount = new Discount(_product,
+                            Argument + "% off",
+                            -quantity * unitPrice * Argument / 100.0);
+
+                        break;
+                    case SpecialOfferType.TwoForAmount:
+                        {
+                            var total = Argument * nbOfPacks + quantityAsInt % 2 * unitPrice;
+                            var discountN = unitPrice * quantity - total;
+                            discount = new Discount(_product, "2 for " + Argument.ToString(Culture), -discountN);
+
+                            break;
+                        }
+                    case SpecialOfferType.FiveForAmount:
+                        {
+                            var discountTotal = unitPrice * quantity -
+                                                (Argument * nbOfPacks + quantityAsInt % 5 * unitPrice);
+                            discount = new Discount(_product,
+                                nbOfProductNecessaryForOffer + " for " + Argument.ToString(Culture),
+                                -discountTotal);
+
+                            break;
+                        }
+                }
+            }
+
+            return discount;
         }
     }
 }
--- SupermarketReceipt-Refactoring-Kata-main/csharp/SupermarketReceipt/ComputeOffer/ShoppingCart.cs
+++ SupermarketReceipt-Refactoring-Kata-main/csharp/SupermarketReceipt/ComputeOffer/ShoppingCart.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Globalization;
 
 namespace SupermarketReceipt.ComputeOffer
 {
@@ -9,8 +8,6 @@ namespace SupermarketReceipt.ComputeOffer
         private readonly List<ProductQuantity> _items = new List<ProductQuantity>();
 
         private readonly Dictionary<Product, double> _productQuantities = new Dictionary<Product, double>();
-
-        private static readonly CultureInfo Culture = CultureInfo.CreateSpecificCulture("en-GB");
 
         public List<ProductQuantity> GetItems()
         {
@@ -38,61 +35,15 @@ namespace SupermarketReceipt.ComputeOffer
 
         public void HandleOffers(Receipt receipt, Dictionary<Product, Offer> offers, SupermarketCatalog catalog)
         {
-            foreach (var p in _productQuantities.Keys)
+            foreach (var product in _productQuantities.Keys)
             {
-                var quantity = _productQuantities[p];
-                var quantityAsInt = (int) quantity;
-                if (offers.ContainsKey(p))
+                var quantity = _productQuantities[product];
+                
+                if (offers.ContainsKey(product))
                 {
-                    var offer = offers[p];
-                    var unitPrice = catalog.GetUnitPrice(p);
-                    Discount discount = null;
-                    var nbOfProductNecessaryForOffer = offer.OfferType switch
-                    {
-                        SpecialOfferType.ThreeForTwo => 3,
-                        SpecialOfferType.TwoForAmount => 2,
-                        SpecialOfferType.FiveForAmount => 5,
-                        _ => 1
-                    };
-
-                    if (offer.IsApplicable(quantityAsInt))
-                    {
-                        int nbOfPacks = quantityAsInt / nbOfProductNecessaryForOffer;
-                        switch (offer.OfferType)
-                        {
-                            case SpecialOfferType.ThreeForTwo:
-                            {
-                                var discountAmount = quantity * unitPrice - (nbOfPacks * 2 * unitPrice + quantityAsInt % 3 * unitPrice);
-                                discount = new Discount(p, "3 for 2", -discountAmount);
-
-                                break;
-                            }
-                            case SpecialOfferType.TenPercentDiscount:
-                                discount = new Discount(p,
-                                    offer.Argument + "% off",
-                                    -quantity * unitPrice * offer.Argument / 100.0);
-
-                                break;
-                            case SpecialOfferType.TwoForAmount:
-                            {
-                                var total = offer.Argument * nbOfPacks + quantityAsInt % 2 * unitPrice;
-                                var discountN = unitPrice * quantity - total;
-                                discount = new Discount(p, "2 for " + offer.Argument.ToString(Culture), -discountN);
-
-                                break;
-                            }
-                            case SpecialOfferType.FiveForAmount:
-                            {
-                                var discountTotal = unitPrice * quantity -
-                                                    (offer.Argument * nbOfPacks + quantityAsInt % 5 * unitPrice);
-                                discount = new Discount(p,
-                                    nbOfProductNecessaryForOffer + " for " + offer.Argument.ToString(Culture),
-                                    -discountTotal);
-
-                                break;
-                            }
-                        }
-                    }
+                    var offer = offers[product];
+                    var unitPrice = catalog.GetUnitPrice(product);
+                    Discount discount = offer.CreateDiscount(quantity, unitPrice);
 
                     if (discount != null) receipt.AddDiscount(discount);
                 }
