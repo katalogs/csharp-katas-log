f9cadef77c66954443edacad566cedfd80d26a04
Kévin Giraud <kevin.giraud@cdbdx.biz>
2023-03-27 17:10:45 +0200

    extract method ManageQuality to item class

--- GildedRose/GildedRose/GildedRose.cs
+++ GildedRose/GildedRose/GildedRose.cs
@@ -2,72 +2,18 @@
 {
     public class GildedRose
     {
-        private const string agedBrie = "Aged Brie";
-        private const string sulfuras = "Sulfuras, Hand of Ragnaros";
-        private const string backstage = "Backstage passes to a TAFKAL80ETC concert";
         private readonly IList<Item> items;
 
         public GildedRose(IList<Item> Items)
         {
-            this.items = Items;
+            items = Items;
         }
 
         public void UpdateQuality()
         {
             foreach (Item item in items)
             {
-                switch (item.Name)
-                {
-                    case agedBrie:
-                        item.IncreaseQuality();
-                        break;
-                    case backstage:
-                        item.IncreaseQuality();
-                        if (item.SellIn < 11)
-                        {
-                            item.IncreaseQuality();
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            item.IncreaseQuality();
-                        }
-                        break;
-                    case sulfuras:
-                        break;
-                    default:
-                        item.DecreaseQuality();
-                        break;
-                }
-
-                switch (item.Name)
-                {
-                    case sulfuras:
-                        break;
-                    case agedBrie:
-                    case backstage:
-                    default:
-                        item.SellIn--;
-                        break;
-                }
-
-                if (item.IsExpired())
-                {
-                    switch (item.Name)
-                    {
-                        case agedBrie:
-                            item.IncreaseQuality();
-                            break;
-                        case backstage:
-                            item.ResetQuality();
-                            break;
-                        case sulfuras:
-                            break;
-                        default:
-                            item.DecreaseQuality();
-                            break;
-                    }
-                }
+                item.ManageQuality();
             }
         }
     }
--- GildedRose/GildedRose/Item.cs
+++ GildedRose/GildedRose/Item.cs
@@ -9,6 +9,10 @@
         private const int qualityMin = 0;
         private const int qualityMax = 50;
 
+        private const string agedBrie = "Aged Brie";
+        private const string sulfuras = "Sulfuras, Hand of Ragnaros";
+        private const string backstage = "Backstage passes to a TAFKAL80ETC concert";
+
         public override string ToString()
         {
             return this.Name + ", " + this.SellIn + ", " + this.Quality;
@@ -38,6 +42,62 @@
         public void ResetQuality()
         {
             Quality = qualityMin;
+        }
+
+        public void ManageQuality()
+        {
+            switch (Name)
+            {
+                case agedBrie:
+                    IncreaseQuality();
+                    break;
+                case backstage:
+                    IncreaseQuality();
+                    if (SellIn < 11)
+                    {
+                        IncreaseQuality();
+                    }
+
+                    if (SellIn < 6)
+                    {
+                        IncreaseQuality();
+                    }
+                    break;
+                case sulfuras:
+                    break;
+                default:
+                    DecreaseQuality();
+                    break;
+            }
+
+            switch (Name)
+            {
+                case sulfuras:
+                    break;
+                case agedBrie:
+                case backstage:
+                default:
+                    SellIn--;
+                    break;
+            }
+
+            if (IsExpired())
+            {
+                switch (Name)
+                {
+                    case agedBrie:
+                        IncreaseQuality();
+                        break;
+                    case backstage:
+                        ResetQuality();
+                        break;
+                    case sulfuras:
+                        break;
+                    default:
+                        DecreaseQuality();
+                        break;
+                }
+            }
         }
     }
 }
