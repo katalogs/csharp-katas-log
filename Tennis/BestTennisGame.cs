9feb5f0d336c42b22b4b1801832217e18f24b476
Angélique Le bail <angelique.lebail@cdbdx.biz>
2023-08-28 11:02:17 +0200

    feat: add BestTennisGame

---
+++ Tennis/BestTennisGame.cs
@@ -0,0 +1,27 @@
+﻿namespace Tennis
+{
+    public class BestTennisGame
+    {
+        private string server;
+        private string receiver;
+        private int serverScore;
+
+        public BestTennisGame(string server, string receiver)
+        {
+            this.server = server;
+            this.receiver = receiver;
+        }
+
+        public string GetScore()
+        {
+            if (serverScore == 1)
+                return "Fifteen-Love";
+            return "Love-All";
+        }
+
+        public void WonPoint(string player)
+        {
+            serverScore += 1;
+        }
+    }
+}
---
+++ Tennis/Tests/BestTennisTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Tennis.Tests
+{
+    public class BestTennisTests
+    {
+        [Fact]
+        public void Should_announce_score_with_love_all_when_game_begins()
+        {
+            var game = new BestTennisGame("Federer", "Nadal");
+            game.GetScore().Should().Be("Love-All");
+        }
+
+        [Fact]
+        public void Should_announce_score_with_fifteen_love_when_server_scores_one_point()
+        {
+            var game = new BestTennisGame("Federer", "Nadal");
+            game.WonPoint("Federer");
+            game.GetScore().Should().Be("Fifteen-Love");
+        }
+    }
+}
