36bf81f152dba5e7ece34020e6e7938ae407d280
Ousmane Bah <ousmane.bah@cdbdx.biz>
2023-09-04 11:11:13 +0200

    Refacto condition

--- Tennis/BestTennisGame.cs
+++ Tennis/BestTennisGame.cs
@@ -5,6 +5,7 @@
         private string server;
         private string receiver;
         private int serverScore;
+        private int receiverScore;
 
         public BestTennisGame(string server, string receiver)
         {
@@ -14,14 +15,40 @@
 
         public string GetScore()
         {
+            if (receiverScore == 1 && serverScore == 1)
+            {
+                return $"{Scores.Fifteen}-{Scores.All}";
+            }
             if (serverScore == 1)
-                return "Fifteen-Love";
-            return "Love-All";
+                return $"{Scores.Fifteen}-{Scores.Love}";
+            if (receiverScore == 1)
+            {
+                return $"{Scores.Love}-{Scores.Fifteen}";
+            }
+            return $"{Scores.Love}-All";
         }
 
         public void WonPoint(string player)
         {
-            serverScore += 1;
+            if (player == receiver)
+            {
+                receiverScore = +1;
+            }
+            else
+            {
+                serverScore += 1;
+            }
         }
     }
+
+    public class Scores
+    {
+
+        public const string Love = "Love";
+
+        public const string Fifteen = "Fifteen";
+
+        public const string All = "All";
+    }
+
 }
--- Tennis/Tests/BestTennisTests.cs
+++ Tennis/Tests/BestTennisTests.cs
@@ -19,5 +19,22 @@ namespace Tennis.Tests
             game.WonPoint("Federer");
             game.GetScore().Should().Be("Fifteen-Love");
         }
+        
+        [Fact]
+        public void Should_announce_score_with_love_fifteen_when_receiver_scores_one_point()
+        {
+            var game = new BestTennisGame("Federer", "Nadal");
+            game.WonPoint("Nadal");
+            game.GetScore().Should().Be("Love-Fifteen");
+        }
+        
+        [Fact]
+        public void Should_announce_score_with_fifteen_all_when_receiver_and_server_scores_one_point()
+        {
+            var game = new BestTennisGame("Federer", "Nadal");
+            game.WonPoint("Nadal");
+            game.WonPoint("Federer");
+            game.GetScore().Should().Be("Fifteen-All");
+        }
     }
 }
