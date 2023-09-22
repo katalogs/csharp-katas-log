697bb4d988a08a60edbc0fca63e5617e157b82b8
Farid Ammar <farid.ammar@cdbdx.biz>
2023-09-22 15:45:31 +0200

    update package nuget Xunit + update 1st test

--- GildedRose/GildedRoseTests/GildedRoseTest.cs
+++ GildedRose/GildedRoseTests/GildedRoseTest.cs
@@ -1,17 +1,27 @@
 using System.Collections.Generic;
+using Argon;
 using GildedRoseKata;
 using Xunit;
 
 namespace GildedRoseTests;
 
+[UsesVerify]
 public class GildedRoseTest
 {
+
+
+    public GildedRoseTest()
+    {
+        VerifierSettings.AddExtraSettings(serializerSettings =>
+                serializerSettings.DefaultValueHandling = DefaultValueHandling.Include);
+    }
+
     [Fact]
-    public void foo()
+    public Task foo()
     {
         IList<Item> Items = new List<Item> {new() {Name = "foo", SellIn = 0, Quality = 0}};
         var gildedRose = new GildedRose(Items);
         gildedRose.UpdateQuality();
-        Assert.Equal("fixme", Items[0].Name);
+        return Verify(Items);
     }
 }
